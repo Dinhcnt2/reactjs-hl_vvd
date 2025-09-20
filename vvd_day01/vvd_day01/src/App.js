29652d183e31c9571291b66ebe9cc11f2db9e5f0
Dinhcnt2 <nho1loaihoa@gmail.com>
2025-09-21 00:20:26 +0700

    Add vvd_day01 files

---
+++ vvd_day01/vvd_day01/src/App.js
@@ -0,0 +1,20 @@
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <h1> Thông tin sinh viên</h1>
+        <hr/>
+        <div className='alert alert-danger'>
+          <p>Mã sinh viên: 22001235</p>
+          <p>Họ và tên: Vương định</p>
+          <p>Email: emkoykkoy39@gmail.com</p>
+          <p>Phone: 0976067728</p>
+        </div>
+    </div>
+  );
+}
+
+export default App;
